7015383bb8d3987869c3cb77775171e198d1e4a7
edio <dev.edioj@gmail.com>
2022-01-21 14:12:21 -0300

    Criação de components, uso props e states

---
+++ frontend/my-app/src/App.js
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+import Header from "./components/Header";
+
+
+function App() {
+  
+  const [projects, setProjects] = useState(['Desenvolvimento de app', 'Front-end web']);
+  
+  function handleAddProject(){
+    
+    setProjects([...projects, `Novo projeto ${Date.now()}`]);
+
+    console.log(projects);
+  }
+
+  return (
+    <>
+    <Header title="Project"/>
+    <ul>{projects.map(project => <li key={project}>{project}</li>)}
+    </ul>
+
+    <button type="button" onClick={handleAddProject}>Adicionar projeto</button>
+    </>
+
+    
+    
+  );
+}
+
+export default App;
